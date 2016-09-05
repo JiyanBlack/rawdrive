42d880b8d2906089201647e798838a0a80fb151f
Blake <rglsm7655558@gmail.com>
2016-09-05 18:54:05 +0800

    official demo

---
+++ index.js
@@ -0,0 +1,14 @@
+/*
+to:
+list -- all accounts
+list
+save dir:
+    /save_dir/accountName1
+    /save_dir/accountName2
+
+class UserAccount()
+    authentication_path=
+    saved_path=
+    sync();
+    
+*/
